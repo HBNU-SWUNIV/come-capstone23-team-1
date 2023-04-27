dfd68ba6c935a8ab67a3ccdad383e512834709c7
Antarctica1 <jhh019176@gmail.com>
2023-04-27 17:18:40 +0900

    update

---
+++ 003 Code/Android/app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton map, music, sleep;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        map = (ImageButton) findViewById(R.id.Map);
+        music = (ImageButton) findViewById(R.id.Music);
+        sleep = (ImageButton) findViewById(R.id.Sleep);
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        sleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SleepActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MusicActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
