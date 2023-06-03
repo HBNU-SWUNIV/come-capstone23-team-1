89d8f8684ff4b2058d43deee55001aff656d6c09
Antarctica1 <jhh019176@gmail.com>
2023-06-03 19:06:21 +0900

    UI 변경

--- 003 Code/Android/app/src/main/java/com/example/project/MainActivity.java
+++ 003 Code/Android/app/src/main/java/com/example/project/MainActivity.java
@@ -2,24 +2,63 @@ package com.example.project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
-    ImageButton map, music, sleep, schedule;
+
+    Button Graph, Map, Calendar, Recoder;
+    private Chronometer chronometer;
+    private Button wake_up;
+    private long pauseOffset;
+    private boolean running;
+    long now = System.currentTimeMillis();
+    Date date = new Date(now);
+    SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd");
+    String formatDate = sdfNow.format(date);
+
+    TextView dateNow;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        map = (ImageButton) findViewById(R.id.Map);
-        music = (ImageButton) findViewById(R.id.Music);
-        sleep = (ImageButton) findViewById(R.id.Sleep);
-        schedule = (ImageButton) findViewById(R.id.schedule);
+        dateNow = (TextView) findViewById(R.id.dateNow);
+        dateNow.setText(formatDate);
 
-        map.setOnClickListener(new View.OnClickListener() {
+        chronometer = findViewById(R.id.chronometer);
+        chronometer.setFormat("%s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+        Graph = (Button)findViewById(R.id.GraphButton);
+        Map = (Button)findViewById(R.id.MapButton);
+        Calendar = (Button)findViewById(R.id.CalendarButton);
+        Recoder = (Button)findViewById(R.id.RecoderButton);
+
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if ((SystemClock.elapsedRealtime() -  chronometer.getBase()) >= 10000) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    Toast.makeText(MainActivity.this, "이제 스마트폰 화면을 꺼두셔도 됩니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MapActivity.class);
@@ -27,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        sleep.setOnClickListener(new View.OnClickListener() {
+        Graph.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SleepActivity.class);
@@ -35,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        music.setOnClickListener(new View.OnClickListener() {
+        Recoder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RecoderActivity.class);
@@ -43,12 +82,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        schedule.setOnClickListener(new View.OnClickListener() {
+        Calendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ScheduleActivity.class);
                 startActivity(intent);
             }
         });
+
     }
+
+
+    public void startChronometer(View v) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void pauseChronometer(View v) {
+        if (running) {
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+
+    public void resetChronometer(View v) {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+    }
+
 }
