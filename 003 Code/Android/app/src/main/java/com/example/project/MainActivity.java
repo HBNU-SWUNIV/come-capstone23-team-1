882138efda9625a52493f03a6f68caace64a6e39
Antarctica1 <jhh019176@gmail.com>
2023-11-10 01:42:15 +0900

    스톱워치 기능 버그 fix

--- 003 Code/Android/app/src/main/java/com/example/project/MainActivity.java
+++ 003 Code/Android/app/src/main/java/com/example/project/MainActivity.java
@@ -47,9 +47,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
                 long time = SystemClock.elapsedRealtime() - chronometer.getBase();
-                int h = (int)(time / 36000000);
-                int m = (int)(time - h*36000000)/60000;
-                int s = (int)(time - h*36000000 - m*60000) / 1000;
+                int h = (int)(time / 3600000);
+                int m = (int)(time - h*3600000)/60000;
+                int s = (int)(time - h*3600000 - m*60000) / 1000;
                 String t = (h < 10 ? "0"+h: h)+":" + (m < 10 ? "0"+m : m) + ":" + (s < 10 ? "0"+s: s);
 
                 chronometer.setText(t);
